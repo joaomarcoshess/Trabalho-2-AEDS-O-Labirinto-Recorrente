c3d9b275c0f53ef76e51bbd340e434dec14cf98e
joaomarcoshess <75141271+joaomarcoshess@users.noreply.github.com>
2023-04-21 14:41:17 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,17 @@ using namespace std;
 
 int main() 
 {
-  Labirinto l(10, 0, 0, 0, 0);
+  
+  Lab l(10, 0, 0, 0, 0);
 
-  l.criandoArquivoAuxiliar();
-  l.criandoArquivoVerificador();
-  l.criandoLabirinto();
-  l.pegandoValoresLabirinto();
-  l.deletandoLabirinto();
+  l.Welcome();
+  l.CriarArqAux();
+  l.CriarArqVerificador();
+  l.CriandoLab();
+  l.PegarValoresLab();
+  l.DeletandoLab();
   cout << endl << "Vitória!!!" << endl;
-  l.printResultados();
+  l.Resultado();
   
   remove("arquivoAuxiliar.txt");
   remove("segundoArquivoAuxiliar.txt");
